a8e86625555eecafd25b0f44f9f20262758c88b2
dacc9 <dr23112002@gmail.com>
2025-07-20 16:12:25 +0200

    nextcloud is functional

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,10 +15,9 @@ provider "libvirt" {
 
 locals {
   root_dir     = abspath("${path.module}/..")
-  image_path   = "${local.root_dir}/project_pool/jammy-server-cloudimg-amd64.img"
+  image_path   = "${local.root_dir}/cloud_image/jammy-server-cloudimg-amd64.img"
   image_exists = fileexists(local.image_path)
   ssh_key      = trimspace(file("${local.root_dir}/ssh_keys/project_key.pub"))
-
   names = ["load_balancer", "next_cloud", "mail_server", "database_server"]
   ips   = ["10.0.0.10", "10.0.0.20", "10.0.0.30", "10.0.0.40"]
   macs  = ["52:54:00:aa:bb:10", "52:54:00:aa:bb:20", "52:54:00:aa:bb:30", "52:54:00:aa:bb:40"]
@@ -30,11 +29,23 @@ resource "libvirt_pool" "pool" {
   path = "${local.root_dir}/project_pool"
 }
 
+resource "null_resource" "fix_pool_permissions" {
+  triggers = {
+    pool_path = libvirt_pool.pool.path
+  }
+
+  provisioner "local_exec" {
+    command = "chmod 755 '${libvirt_pool.pool.path}'"
+  }
+
+  depends_on = [libvirt_pool.pool]
+}
+
 resource "null_resource" "download_image" {
   count = local.image_exists ? 0 : 1
-
   provisioner "local-exec" {
     command = <<EOT
+      mkdir -p "${local.root_dir}/cloud_image"
       wget -q -nc "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img" \
         -O "${local.image_path}"
     EOT
@@ -61,7 +72,6 @@ data "cloudinit_config" "vm" {
   count          = length(local.names)
   gzip           = false
   base64_encode  = false
-
   part {
     content_type = "text/cloud-config"
     content = templatefile("${path.module}/cloud-init/user-data.yml", {
@@ -77,7 +87,6 @@ resource "libvirt_cloudinit_disk" "cidata" {
   name      = "${local.names[count.index]}-init.iso"
   pool      = libvirt_pool.pool.name
   user_data = data.cloudinit_config.vm[count.index].rendered
-
   network_config = yamlencode({
     version = 2
     ethernets = {
@@ -99,7 +108,6 @@ resource "libvirt_cloudinit_disk" "cidata" {
       }
     }
   })
-
   meta_data = yamlencode({
     instance-id     = local.names[count.index]
     local-hostname  = local.names[count.index]
@@ -111,7 +119,7 @@ resource "libvirt_network" "net" {
   mode      = "nat"
   domain    = "example.local"
   addresses = ["10.0.0.0/24"]
-
+  autostart = true
   dhcp {
     enabled = false
   }
@@ -122,30 +130,24 @@ resource "libvirt_domain" "vm" {
   name   = local.names[count.index]
   vcpu   = 2
   memory = 2048
-
   disk {
     volume_id = libvirt_volume.disk[count.index].id
   }
-
   network_interface {
     network_id = libvirt_network.net.id
     mac        = local.macs[count.index]
   }
-
   cloudinit = libvirt_cloudinit_disk.cidata[count.index].id
-
   console {
     type        = "pty"
     target_type = "serial"
     target_port = "0"
   }
-
   console {
     type        = "pty"
     target_type = "virtio"
     target_port = "1"
   }
-
   graphics {
     type        = "spice"
     listen_type = "address"
@@ -159,4 +161,3 @@ output "vm_ips" {
     local.names[idx] => local.ips[idx]
   }
 }
-
